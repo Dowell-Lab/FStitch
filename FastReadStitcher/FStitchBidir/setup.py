135bfe23f8a5d01bf682d8baf193b6aa07a628c4
Margaret Gruca <mgruca@cu-biot-2-10.203.132.108.int.colorado.edu>
2018-11-30 10:10:11 -0700

    Added setup.py for FStitchBidir python module

---
+++ FastReadStitcher/FStitchBidir/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+#with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+#    long_description = f.read()
+
+setup(name='FStitchBidir',
+      version='0.1',
+      description='Annotates bidirections using FStitch segment data.',
+      url='http://github.com/storborg/funniest',
+      author='Margaret Gruca',
+      author_email='margaret.gruca@colorado.edu',
+      license='MIT'
+      classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Programming Language :: Python :: 3'])
+
+keywords='bioinformatics genomics chromatin ATAC-seq motif transcription_factor',
+
+    package_dir={'bidir' : 'bidir'},
+    packages=['FStitchBidir'],
+
+    install_requires=[
+        'argparse',
+        'datetime',
+        'numpy',
+        'pybedtools',
+        'chartify',
+        'pandas',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'bidir=FStitch:bidir',
+        ],
+    },
+
+    project_urls={
+        'Bug Reports': 'https://github.com/Dowell-Lab/FStitch',
+        'Source': 'https://github.com/Dowell-Lab/FStitch',
+        'Human RefSeq Gene Annotation Download': 'http://genome.ucsc.edu/cgi-bin/hgTables',
+    },
+)
